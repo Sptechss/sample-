1c7ac761eaba4784f773e17f11a740556bfef435
sahil shaikh <sahilshaikh70034@gmail.com>
2022-09-24 12:52:47 +0530

    inserted complted

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -4,32 +4,39 @@ namespace App\Controllers;
 
 class Home extends BaseController
 {
-    public function index($id=null)
-    {   
+    public function index()
+    {
         $db      = \Config\Database::connect();
         $builder  = $db->table('records');
         $data = [
-            'name'=>'sahil khan',
-            'email'=>'sahilshaikh@70034gmail.com',
-            'address'=>'beed by pass road',
-            'phone'=>'8788946164'
+            [
+                'id'=>'2',
+                'name' => 'fatima shaikh',
+                'email' => 'fatima@gmail.com',
+                'address' => 'pass road',
+                'phone' => '87845456164',
+            ], [
+                'id'=>'3',
+                'name' => 'sankit patil',
+                'email' => 'sankit@gmail.com',
+                'address' => 'jalna by pass road',
+                'phone' => '7788946162',
+            ]
         ];
-         $builder->update($id,$data);
+        $builder->insert($data);
         $data['page'] = view('frontend/home');
-        return view('frontend/template',$data);
+        return view('frontend/template', $data);
     }
 
     public function about()
     {
         $data['page'] = view('frontend/about');
-        return view('frontend/template',$data);
+        return view('frontend/template', $data);
     }
 
     public function load()
     {
         $data['page'] = view('frontend/load');
-        return view('frontend/template',$data);
+        return view('frontend/template', $data);
     }
 }
-
-
